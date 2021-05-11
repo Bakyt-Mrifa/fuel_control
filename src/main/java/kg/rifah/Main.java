6122090c404c024b95d26f76e9a0029d50182bc2
Мамбеталиев Бакыт Мэлисович <bmambetaliev@megacom.local>
2021-05-11 11:21:07 +0600

    Fuel Calculator

--- src/main/java/kg/rifah/Main.java
+++ src/main/java/kg/rifah/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
        // mainStage.initStyle(StageStyle.UNDECORATED);
         mainStage.show();
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- src/main/java/kg/rifah/exception/FillErrorException.java
+++
@@ -1,4 +0,0 @@
-package kg.rifah.exception;
-
-public class FillErrorException {
-}
